7d09f41c2acb484e0a92ee3c6b391c4f3200ffec
vladutclp <vladut.clapou@gmail.com>
2020-06-24 21:48:46 +0300

    First Navbar style finished

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.get("/", (req, res) =>{
+  
+})
+
 app.get('/api/customers', (req, res) =>{
   const customers = [
     {id: 1, firstName: 'Jaxon', secondName: 'Koxeanu'},
     {id: 2, firstName: 'Fisteus', secondName: 'Calithos'}
   ];
-
+  console.log("This route was reached");
   res.json(customers);
 });
 
